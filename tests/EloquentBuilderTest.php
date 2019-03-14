211c353b1862afb12e35b6bf47d7d1bcfb2da8f9
fouladgar.dev <fouladgar.dev@gmail.com>
2019-03-14 11:39:41 +0330

    Add Laravel 5.8 support

--- tests/EloquentBuilderTest.php
+++ tests/EloquentBuilderTest.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Builder;
 
 class EloquentBuilderTest extends TestCase
 {
-    public function setUp()
+    public function setUp(): void
     {
         parent::setUp();
 
